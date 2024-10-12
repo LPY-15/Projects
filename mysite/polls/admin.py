8d83d9a30c75b48cd462f937011d60abf453351a
Kirill <befordshir@gmail.com>
2024-10-12 19:00:07 +0300

    123

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
-# C:\Projects\mysite
+# C:\Projects\1_project\mysite
 
 class ChoiceInLine(admin.TabularInline):
     model = Choice
